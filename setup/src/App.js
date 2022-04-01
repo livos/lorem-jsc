d3bad04726be35c545bca1768fec36bbd8689e29
livos <lvivancos2nd-dev@yahoo.fr>
2022-04-01 18:38:52 +0700

    base setup

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,11 +1,40 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handdleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello world");
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form onSubmit="handleSubmit" className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, vel?
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
