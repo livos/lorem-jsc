67705ed684f6b251125ad8a569d322ab1d1d0847
livos <lvivancos2nd-dev@yahoo.fr>
2022-04-01 18:49:58 +0700

    generate paragraphs

--- setup/src/App.js
+++ setup/src/App.js
@@ -4,15 +4,22 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
-  const handdleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
-      <form onSubmit="handleSubmit" className="lorem-form">
+      <form onSubmit={handleSubmit} className="lorem-form">
         <label htmlFor="amount">paragraphs:</label>
         <input
           type="number"
@@ -26,12 +33,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, vel?
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
